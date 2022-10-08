c3ac911954746908e4727ff241fbd97500511795
Andreanapolicy <daa.and@mail.ru>
2022-10-08 12:40:47 +0300

    Lab1. Добавил описание автомата

---
+++ lab1/Mealy-Moore/src/Common/Automat.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <optional>
+
+enum class MachineType
+{
+    Mealy,
+    Moore
+};
+
+struct MachineState
+{
+    std::string state;
+    std::optional<std::string> outputData;
+};
+
+struct Machine
+{
+    std::vector<std::string> inputData;
+    std::vector<std::vector<MachineState>> machineStates;
+    std::vector<std::string> outputData;
+};
