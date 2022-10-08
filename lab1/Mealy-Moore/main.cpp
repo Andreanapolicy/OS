62e7c10a0a8edc47759cc1fd470118135809deb1
Andreanapolicy <daa.and@mail.ru>
2022-10-08 12:54:58 +0300

    Lab1. Реализовал парс данных командной строки

--- lab1/Mealy-Moore/main.cpp
+++ lab1/Mealy-Moore/main.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <optional>
+#include "src/Common/Automat.h"
 
-int main()
+struct Args
 {
-    std::cout << "Hello, World!" << std::endl;
+    MachineType machineType;
+    std::string inputFile;
+    std::string outputFile;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+    Args args = {};
+    const std::string MEALY_MOORE_COMMAND = "mealy-to-moore";
+    const std::string MOORE_MEALY_COMMAND = "moore-to-mealy";
+
+    if (argc != 4)
+    {
+        return std::nullopt;
+    }
+
+    if (argv[1] == MEALY_MOORE_COMMAND)
+    {
+        args.machineType = MachineType::Mealy;
+    }
+    else if (argv[1] == MOORE_MEALY_COMMAND)
+    {
+        args.machineType = MachineType::Moore;
+    }
+    else
+    {
+        return std::nullopt;
+    }
+
+    args.inputFile = argv[2];
+    args.outputFile = argv[3];
+    return args;
+}
+
+int main(int argc, char* argv[])
+{
+    auto args = ParseArgs(argc, argv);
+    if (!args)
+    {
+        std::cout << "Wrong usage. Example: .exe mealy-to-moore input.csv output.csv" << std::endl;
+        return 1;
+    }
+
     return 0;
 }
