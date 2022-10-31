0bcbb9d29a7fe011d11504afa1dd359c25682a4a
Andreanapolicy <daa.and@mail.ru>
2022-10-31 19:07:36 +0300

    Lab3. Добавил получение всех терминальных и нетерминальных символов для 1 состояния

--- lab3/GrammarToDSM/src/GrammarParser/GrammarParser.cpp
+++ lab3/GrammarToDSM/src/GrammarParser/GrammarParser.cpp
@@ -1,4 +1,53 @@
 #include "GrammarParser.h"
+#include "../Common/Utils.h"
+
+namespace
+{
+    struct Transition
+    {
+        std::string state;
+        std::string inputData;
+    };
+
+    struct StatesGrammar
+    {
+        std::string originState;
+        std::vector<Transition> transitions;
+    };
+
+    void FillTransitions(std::vector<Transition>& transitions, std::istream& input, bool isLeftSideGrammar)
+    {
+        std::string transition;
+
+        while (std::getline(input, transition, '|'))
+        {
+            std::string state;
+            std::string inputData;
+            auto index = 0;
+
+            transition = trim(transition);
+            if (transition.size() == 1)
+            {
+                transitions.emplace_back(Transition{"", transition});
+
+                continue;
+            }
+
+            if (isLeftSideGrammar)
+            {
+                state = transition.substr(0, 1);
+                inputData = transition.substr(1, transition.length() - 1);
+            }
+            else
+            {
+                state = transition.substr(1, transition.length() - 1);
+                inputData = transition.substr(0, 1);
+            }
+
+            transitions.emplace_back(Transition{state, inputData});
+        }
+    }
+}
 
 Machine GrammarParser::ParseGrammarToMachine(std::istream& input, GrammarSide grammarSide)
 {
@@ -6,12 +55,29 @@ Machine GrammarParser::ParseGrammarToMachine(std::istream& input, GrammarSide gr
     grammarSide == GrammarSide::RIGHT
         ? ParseRightGrammarSideToMachine(machine, input)
         : ParseLeftGrammarSideToMachine(machine, input);
-    
+
     return machine;
 }
 
 Machine GrammarParser::ParseLeftGrammarSideToMachine(Machine& machine, std::istream& input)
 {
+    std::string line;
+    while (std::getline(input, line))
+    {
+        std::istringstream iss(line);
+        std::string state;
+        std::string trash;
+        iss >> state;
+        machine.inputData.emplace_back(state);
+
+        iss >> trash;
+
+        std::vector<Transition> transitions;
+        FillTransitions(transitions, iss, true);
+
+        //auto stateGrammar = GetStateGrammar(input);
+    }
+
     return Machine();
 }
 
