f3aff6ec8305d1db637ee2e473458e88291f5d11
Andreanapolicy <daa.and@mail.ru>
2022-11-07 12:11:56 +0300

    Lab3.2. Поменял структуру автомата. Начал рефакторинг парсинга

--- lab3/NotDSMToDSM/src/Machine/Machine.h
+++ lab3/NotDSMToDSM/src/Machine/Machine.h
@@ -2,29 +2,22 @@
 
 #include <vector>
 #include <string>
-#include <optional>
-
-enum class MachineType
-{
-    Mealy,
-    Moore
-};
+#include <set>
 
 struct MachineState
 {
     std::string state;
-    std::optional<std::string> outputData = std::nullopt;
+    bool isFinal = false;
+};
 
-    bool operator==(const MachineState& machineState) const
-    {
-        return this->state == machineState.state && this->outputData == machineState.outputData;
-    }
+struct MachineTransitionState
+{
+    std::set<std::string> state;
 };
 
 struct Machine
 {
     std::vector<std::string> inputData;
-    std::vector<std::string> states;
-    std::vector<std::vector<MachineState>> machineStates;
-    std::vector<std::string> outputData;
+    std::vector<MachineState> states;
+    std::vector<std::vector<MachineTransitionState>> machineStates;
 };
